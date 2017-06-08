1f070e4c03b19d3bd7223a526bd2df73ac367eb9
obellavance <bellavance.o@gmail.com>
2017-06-07 23:56:25 -0400

    yo je sais comment faire

--- back-end/AppServer/src/AddResult.java
+++ back-end/AppServer/src/AddResult.java
@@ -6,16 +6,16 @@ import java.sql.Statement;
  * Created by Olivier on 2017-06-05.
  */
 public class AddResult {
-    private String url = "jdbc:postgresql://s6ie1702.gel.usherbrooke.ca:5432/postgres";
-    private String user = "administrateur";
-    private String passwd = "s6infoe17";
+    private static String url = "jdbc:postgresql://s6ie1702.gel.usherbrooke.ca:5432/postgres";
+    private static String user = "administrateur";
+    private static String passwd = "s6infoe17";
 
     public static void main(String[] args){
-        AddResult Connect1 = new AddResult();
-        Connect1.insertResult(1, 2, 1,90,"test", 1,3,2,4);
+       // AddResult Connect1 = new AddResult();
+       // Connect1.insertResult(1, 2, 1,90,"test", 1,3,2,4);
     }
 
-    public void insertResult(int doc_1, int doc_2,int methode, int pourcentage, String commentaire,int debut1, int fin1, int debut2, int fin2 ) {
+    public static void insertResult(int doc_1, int doc_2,int methode, int pourcentage, String commentaire,int debut1, int fin1, int debut2, int fin2 ) {
         try {
             Class.forName("org.postgresql.Driver");
             Connection conn = DriverManager.getConnection(url, user, passwd);
