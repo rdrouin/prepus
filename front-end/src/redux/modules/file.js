3d74a93f0fdd7a64df63d81569d88dbf3c2822f3
Alexandre Buche <Alexandre Buche>
2017-06-08 09:46:25 -0400

    API en JSON

--- front-end/src/redux/modules/file.js
+++ front-end/src/redux/modules/file.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 const xmlData = `<depot>
 <id>1</id>
 <files>
@@ -33,6 +34,12 @@ const xmlData = `<depot>
 </similarity>
 </similarities>
 </depot>`
+=======
+//import fetch from 'isomorphic-fetch'
+
+const jsonData = `{"depot":{"id" : "1","files" : [{"id" : "1","name" : "Salut"},{"id" : "2","name" : "Buche"},{"id" : "5","name" : "Tes"},{"id" : "6","name" : "Beau"}],"similarities" : [{"file1" : "1","file2" : "2","percent" : "3","type"  : "4"},{"file1" : "5","file2" : "6","percent" : "7","type"  : "8"}]}}
+`
+>>>>>>> API en JSON
 
 // ------------------------------------
 // Constants
@@ -43,7 +50,17 @@ export const SET_ACTIVE_FILE_RIGHT = 'SET_ACTIVE_FILE_RIGHT'
 export const REMOVE_ACTIVE_FILE_LEFT = 'REMOVE_ACTIVE_FILE_LEFT'
 export const REMOVE_ACTIVE_FILE_RIGHT = 'REMOVE_ACTIVE_FILE_RIGHT'
 export const REMOVE_ACTIVE_FILES = 'REMOVE_ACTIVE_FILES'
+<<<<<<< HEAD
 export const LOAD_DEPOT = 'LOAD_DEPOT'
+<<<<<<< HEAD
+=======
+export const SHOW_SIMILARITIES_ONLY = 'SHOW_SIMILARITIES_ONLY'
+=======
+export const RECEIVE_FILES = 'RECEIVE_FILES'
+export const REQUEST_FILE = 'REQUEST_FILE'
+export const REQUEST_ANALYSIS = 'REQUEST_ANALYSIS'
+>>>>>>> API en JSON
+>>>>>>> API en JSON
 
 // ------------------------------------
 // Actions
@@ -62,7 +79,12 @@ function append(id, cip, name, size, plagiarism) {
   }
 }
 
+<<<<<<< HEAD
 function loadDepot(){
+=======
+<<<<<<< HEAD
+function loadDepot() {
+>>>>>>> API en JSON
   return {
     type: LOAD_DEPOT,
     payload: {
@@ -71,6 +93,8 @@ function loadDepot(){
   }
 }
 
+=======
+>>>>>>> API en JSON
 function setActiveFileLeft(id) {
   return {
     type: SET_ACTIVE_FILE_LEFT,
@@ -107,6 +131,26 @@ function removeActiveFiles() {
   }
 }
 
+function receiveFiles(response){
+  return {
+    type: RECEIVE_FILES,
+    payload: response
+  }
+}
+
+function requestFiles() {
+  return {
+    type: REQUEST_FILE
+  }
+}
+
+function requestAnalysis() {
+  return {
+    type: REQUEST_ANALYSIS
+  }
+}
+
+
 export const FileActions = {
   append,
   setActiveFileLeft,
@@ -114,7 +158,13 @@ export const FileActions = {
   removeActiveFileLeft,
   removeActiveFileRight,
   removeActiveFiles,
+<<<<<<< HEAD
   loadDepot
+=======
+  loadDepot,
+  showSimilaritiesOnly,
+  analyseDepot
+>>>>>>> API en JSON
 }
 
 // ------------------------------------
@@ -127,6 +177,7 @@ const ACTION_HANDLERS = {
   [REMOVE_ACTIVE_FILE_LEFT]: (state, action) => ({ ...state, activeFileLeft: -1 }),
   [REMOVE_ACTIVE_FILE_RIGHT]: (state, action) => ({ ...state, activeFileRight: -1 }),
   [REMOVE_ACTIVE_FILES]: (state, action) => ({ ...state, activeFileRight: -1, activeFileLeft: -1 }),
+<<<<<<< HEAD
   [LOAD_DEPOT]: (state, action) => (bs(state)),
 }
 
@@ -152,9 +203,70 @@ function bs(state){
 
     console.log({...state, files: [...state.files.concat(...newState)]})
   })
+<<<<<<< HEAD
   return {...state, files: [...state.files.concat(...newState)]}
+=======
+  return { ...state, files: [...state.files.concat(...newState)] }
+=======
+  [REQUEST_FILE]: (state, action) => ({...state}),
+  [REQUEST_ANALYSIS]: (state, action) => ({...state}),
+  [RECEIVE_FILES]: (state, action) => (bs2(state, action)),
 }
 
+
+/*
+function loadDepot(){
+  return dispatch => {
+    dispatch(requestFiles())
+    return fetch(`https://api.github.com/users/mralexgray/repos`, {method: 'GET'})
+    .then(response => response.json())
+    .then(json => dispatch(receiveFiles(json)))
+  }
+}*/
+
+function analyseDepot() {
+  return dispatch => {
+    dispatch(requestAnalysis())
+    return fetch(`http://s6ie1702.gel.usherbrooke.ca:8080/appserver/analysis?depot=1`, {method: 'POST'})
+  }
+>>>>>>> API en JSON
+}
+
+function loadDepot(){
+  return dispatch => {
+    dispatch(requestFiles())
+    return fetch(`http://s6ie1702.gel.usherbrooke.ca:8080/appserver/depot`, {method: 'GET'})
+    .then(response => response.json())
+    .then(json => dispatch(receiveFiles(json)))
+  }
+>>>>>>> API en JSON
+}
+
+//dispatch(receiveFiles(response))
+function bs2(state, action) {
+  var newState = []
+  var response = action.payload
+
+  console.log("SERVER DATA!!")
+  console.log(response)
+  var files = response.depot.files
+  var similarities = response.depot.similarities
+  for(var i = 0;i<files.length;i++){
+    newState[i] = {id:files[i].id, name:files[i].name, similarities: []}
+  }
+
+  for(var i = 0;i<similarities.length;i++){
+    var file1 = similarities[i].file1
+    var file2 = similarities[i].file2
+    var percent = similarities[i].percent
+
+    newState.filter(file => file.id  == file1)[0].similarities.push({id:file2, percent: percent})
+    newState.filter(file => file.id  == file2)[0].similarities.push({id:file1, percent: percent})
+  }
+  console.log("newState")
+  console.log(newState)
+  return {...state, files: [...state.files.concat(...newState)]}
+}
 // ------------------------------------
 // Reducer
 // ------------------------------------
