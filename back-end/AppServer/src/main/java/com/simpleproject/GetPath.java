67a7bc7d2d9ca9d8d54e855025800ea12a831783
obellavance <bellavance.o@gmail.com>
2017-06-28 12:31:10 -0400

    fichier de config contenant le path des dossiers

---
+++ back-end/AppServer/src/main/java/com/simpleproject/GetPath.java
@@ -0,0 +1,30 @@
+package com.simpleproject;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
+/**
+ * Created by Olivier on 2017-06-28.
+ */
+public class GetPath {
+
+    public static String path(){
+        Properties properties = new Properties();
+        try {
+            System.out.println("Working Directory = " +
+                    System.getProperty("user.dir"));
+            ClassLoader classLoader = InsertIntoElasticSearch.class.getClassLoader();
+            FileInputStream fileStream = new FileInputStream(classLoader.getResource("config.properties").getFile());
+            properties.load(fileStream);
+
+            fileStream.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return properties.getProperty("document.path");
+    }
+}
