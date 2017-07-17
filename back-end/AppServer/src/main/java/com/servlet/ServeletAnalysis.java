9f07140be2c0d729ba531744b740feeac0a31f11
obellavance <bellavance.o@gmail.com>
2017-07-17 14:13:18 -0400

    modification des paramettre d'envoie de depot

--- back-end/AppServer/src/main/java/com/servlet/ServeletAnalysis.java
+++ back-end/AppServer/src/main/java/com/servlet/ServeletAnalysis.java
@@ -17,13 +17,13 @@ import main.java.com.simpleproject.FileAnalysis;
 public class ServeletAnalysis extends HttpServlet {
     public void doPost(HttpServletRequest req, HttpServletResponse res)
             throws ServletException, IOException {
-        String id = req.getParameter("depot");
-        InsertIntoElasticSearch.encoder(Integer.parseInt(id));
+        String id_Depot = req.getParameter("depot");
+        InsertIntoElasticSearch.encoder(Integer.parseInt(id_Depot));
         //FileAnalysis.launchAnalysis(Integer.parseInt(id));
         res.setContentType("text/plain");
         res.setCharacterEncoding( "UTF-8" );
         PrintWriter out = res.getWriter();
-        out.println(id);
+        out.println(id_Depot);
 
     }
 
