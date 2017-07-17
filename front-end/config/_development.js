b5434b9c53372b052dd8525bdb8b387b3c20746b
Alexandre Buche <Alexandre Buche>
2017-07-17 17:10:03 -0400

    Fix config and lint error

--- front-end/config/_development.js
+++ front-end/config/_development.js
@@ -3,6 +3,8 @@
 // http://stackoverflow.com/questions/34133808/webpack-ots-parsing-error-loading-fonts/34133809#34133809
 export default (config) => ({
   compiler_public_path: `http://${config.server_host}:${config.server_port}/`,
+  server_host: process.env.HOST || 'localhost',
+  server_port: process.env.PORT || 5000,
   proxy: {
     enabled: false,
     options: {
